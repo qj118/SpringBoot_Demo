2fa1a5ec58f3556584ccf107b6e3ddbf4d565a3d
jin.qu <135885@moon-tech.com>
2020-09-09 09:57:29 +0800

    hello-world

---
+++ README.md
@@ -0,0 +1,9 @@
+## 工程简介
+
+Spring Boot 边学边敲——[视频教程](https://www.bilibili.com/video/BV1Et411Y7tQ?p=1)
+
+## 模块简介
+
+### hello-world
+
+体验 Spring Boot 工程的创建过程。
---
+++ hello-world/src/main/java/org/demon/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package org.demon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        // Spring 应用启动
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
