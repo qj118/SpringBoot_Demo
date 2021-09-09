6a869eaac9d7228f790c2bc846c2836df0c1e87e
jin.qu <135885@moon-tech.com>
2021-09-09 13:53:50 +0800

    获取系统配置

--- 02-config/src/main/java/org/demon/ConfigApplication.java
+++ 02-config/src/main/java/org/demon/ConfigApplication.java
@@ -2,14 +2,24 @@ package org.demon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Map;
 
 @ImportResource({"classpath:bean.xml"})
 @SpringBootApplication
 public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(ConfigApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        Map<String, Object> systemEnvironment = environment.getSystemEnvironment();
+        Map<String, Object> systemProperties = environment.getSystemProperties();
+
+        System.out.println(systemEnvironment);
+        System.out.println(systemProperties);
     }
 
 }
